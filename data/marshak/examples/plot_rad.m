bf8fe886a061fad95aa3bf8180118bb51b06b320
XiZhe Xie <1102118111@qq.com>
2025-12-08 12:39:27 +0800

    update: replace marshak folder with latest local version

--- data/marshak/examples/plot_rad.m
+++
@@ -1,6 +0,0 @@
-res = load('result');
-U_rad = res(:,2);
-a = 0.01372;
-c = 29.98;
-T_rad = sqrt(sqrt(U_rad/a/c));
-plot(res(:,1), T_rad)
--- data/marshak/examples/run.sh
+++
@@ -1,4 +0,0 @@
-# !bin/sh 
-
-# exefilename leftboundary rightboundary meshsize endtime nameofresultfile 
-./test1 0 2 400 0.5 result > time_0p5
